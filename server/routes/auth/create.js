993e420943c5af017a03e4b6676523df66292772
angeltveit <torbjorn@angeltveit.info>
2020-07-01 20:46:21 +0200

    phone and jwt

---
+++ server/routes/auth/create.js
@@ -0,0 +1,83 @@
+import Twilio from 'twilio'
+import { mongo } from '../../services/mongo.js'
+import api from '../../middleware/api.js'
+import randomInt from 'random-int'
+import jwt from 'jsonwebtoken'
+
+const TWILIO_SID = process.env.TWILIO_SID
+const TWILIO_SECRET = process.env.TWILIO_SECRET
+
+const twilio = Twilio(TWILIO_SID, TWILIO_SECRET)
+
+async function clearOtp(phone) {
+  await mongo('users').update({
+    phone,
+  },{
+    $set: {
+      otp: null
+    },
+  })
+}
+
+async function authorize(phone, code) {
+
+  const ok = await mongo('users').findOne({
+    phone,
+    otp: code,
+  })
+  
+  await clearOtp(phone)
+  
+  if(!ok) {
+    throw new Error('unauthorized')
+  }
+  return ok
+}
+
+export default [
+  api(async function createMeal(req, res, next) {
+
+    const { phone, code } = req.body
+
+    if(!phone) {
+      throw new Error('phone missing')
+    }
+
+    const otp = randomInt(10000, 99999)
+
+    const exists = await mongo('users').findOne({
+      phone,
+    })
+
+    if(!exists) {
+      await mongo('users').insert({
+        phone,
+      })
+    }
+
+    if(code) {
+      const user = await authorize(phone, code)
+      delete user.otp
+      return {
+        ...user,
+        token: jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '365d' })
+      }
+    }
+    
+    await mongo('users').update({
+      phone,
+    },{
+      $set: {
+        otp,
+      }
+    })
+
+    const { sid } = twilio.messages.create({
+      body: `One time code: ${otp}`,
+      from: 'PortionCtrl',
+      to: `+47${phone}`
+    })
+    
+    return { state: 'otp' }
+  }),
+]
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,10 @@ import Router from 'express/lib/router/index.js'
 import meals from './meals/index.js'
 import ingredients from './ingredients/index.js'
 import recipes from './recipes/index.js'
-
+import auth from './auth/index.js'
 
 export default new Router()
   .use('/meals', meals)
   .use('/ingredients', ingredients)
   .use('/recipes', recipes)
+  .use('/auth', auth)
